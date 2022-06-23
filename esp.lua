1ab6b86e8362e3648f2701d88ea7847d3913fb0c
LURKLURKLURKLURKLURKLURKLURKLURK <96763530+LURKLURKLURKLURKLURKLURKLURKLURK@users.noreply.github.com>
2022-06-23 16:18:32 -0400

    Update esp.lua

--- esp.lua
+++ esp.lua
@@ -1,9 +1,4 @@
--- idc if you use this but please credit me
 --[[
-    [Credits]:
-    Cripware: Box Sizing and Box positioning 
-    IonHub: inspo 
-    
     [TODO]:
     
     Add needed functions: 🟢️
@@ -42,38 +37,38 @@ local CF = CFrame.new
 local Vec3 = Vector3.new
 local Vec2 = Vector2.new
 local new_drawing = Drawing.new
-local wtsp = Camera.WorldToScreenPoint 
+local WTSP = Camera.WorldToScreenPoint 
 local wtvp = Camera.WorldToViewportPoint
 local rgb = Color3.fromRGB
 local find_first_child = game.FindFirstChild
 local find_first_child_of_class = game.FindFirstChildOfClass
 local m_floor = math.floor
 local i_new = Instance.new 
+local GBB = workspace.GetBoundingBox
 
 local esp = { 
-    enabled = false,
+    enabled = true,
     players = true, 
     max_distance_players = 10000, 
     max_distance_objects = 5000,
-    box = {enabled = false, color = rgb(255,255,255), outline = false},
-    inner_box = {enabled = false, color = rgb(0,0,0),transparency = 0.3},
-    name = {enabled = false, color = rgb(255,255,255), outline = false},
-    distance = {enabled = false, color = rgb(255,255,255), outline = false},
-    health = {enabled = false, color = rgb(0,255,0), outline = false},
-    healthbar = {outline = false, enabled = false, higher = rgb(0,255,0), lower = rgb(255,0,0)},
-    tool = {enabled = false, color = rgb(255,255,255), outline = false},
+    box = {enabled = true, color = rgb(255,255,255), outline = true},
+    inner_box = {enabled = true, color = rgb(0,0,0),transparency = 0.3},
+    name = {enabled = true, color = rgb(255,255,255), outline = true},
+    distance = {enabled = true, color = rgb(255,255,255), outline = true},
+    health = {enabled = true, color = rgb(0,255,0), outline = true},
+    healthbar = {outline = true, enabled = true, higher = rgb(0,255,0), lower = rgb(255,0,0)},
+    tool = {enabled = true, color = rgb(255,255,255), outline = true},
     tracers = {enabled = false, color = rgb(255,255,255)},
-    highlights = {fillcolor = rgb(255,255,255), chams = false, enabled = false, visible = rgb(0,255,0),non_visible = rgb(255,0,0), outline_color = rgb(0,0,0), fill_transparency = 0.5, outline_transparency = 0},
+    highlights = {fillcolor = rgb(86,0,232), enabled = true, outline_color = rgb(0,0,0), fill_transparency = 0.5, outline_transparency = 0},
     objects = {},
     visible_check = false,
     font = 2,
     fontsize = 13,
-    measurement = 'studs',
+    measurement = 'meters',
 }
 
-getgenv().esp = esp 
-local utils = {}; do
-    function utils.draw(class, properties)
+do
+    function esp.draw(class, properties)
         local drawing = new_drawing(class)
         for i,v in pairs(properties) do
             drawing[i] = v
@@ -81,7 +76,7 @@ local utils = {}; do
         return drawing
     end
     
-    function utils.instance(class,properties)
+    function esp.instance(class,properties)
         local instance = i_new(class)
         for i,v in pairs(properties) do
             instance[i] = v
@@ -89,23 +84,11 @@ local utils = {}; do
         return instance
     end
 
-    function utils.gethealth(obj)
-        local Health = {} 
-        if obj:IsA('Model') and find_first_child(obj,'Humanoid') then -- adds support for custom things
-            health.health = find_first_child(obj,'Humanoid').Health
-            health.maxhealth = find_first_child(obj,'Humanoid').MaxHealth 
-            return Health
-        elseif obj:IsA('Player') then 
-            health.health = find_first_child(obj.Character,'Humanoid').Health 
-            health.maxhealth = find_first_child(obj.Character,'Humanoid').Health
-        end
-    end
-    
-    function utils.get_magnitude(obj_1,obj_2)
+    function esp.get_magnitude(obj_1,obj_2)
         return (obj_1 - obj_2).Magnitude
     end
     
-    function utils.remove_esp(obj)
+    function esp.remove_esp(obj)
         if rawget(esp.objects, obj) then
             for _, drawing in next, esp.objects[obj] do
                 drawing:Remove()
@@ -114,13 +97,20 @@ local utils = {}; do
         end
     end
     
-    function utils.add_esp_player(obj)
+    function esp.add_esp_player(obj)
         local drawn_objects = {} 
-        drawn_objects.Highlight = utils.instance('Highlight',{
+        drawn_objects.Highlight = esp.instance('Highlight',{
             DepthMode = 'AlwaysOnTop',
             Enabled = false,
         })
-        drawn_objects.Name = utils.draw('Text',{
+        drawn_objects.inner_box = esp.draw('Square',{
+            Visible = false, 
+            Size = Vec2(0,0),
+            Filled = true,
+            Thickness = 0,
+            Color = esp.inner_box.color
+        });
+        drawn_objects.Name = esp.draw('Text',{
             Font = esp.font, 
             Size = esp.size, 
             Text = '',
@@ -128,7 +118,7 @@ local utils = {}; do
             Color = esp.name.color,
             Center = true,
         });
-        drawn_objects.Distance = utils.draw('Text',{
+        drawn_objects.Distance = esp.draw('Text',{
             Font = esp.font, 
             Size = esp.size, 
             Text = '',
@@ -136,7 +126,7 @@ local utils = {}; do
             Color = esp.distance.color,
             Center = true, 
         });
-        drawn_objects.Tool = utils.draw('Text',{
+        drawn_objects.Tool = esp.draw('Text',{
             Font = esp.font, 
             Size = esp.size, 
             Text = '',
@@ -144,53 +134,58 @@ local utils = {}; do
             Color = esp.tool.color,
             Center = true,
         })
-        drawn_objects.Box_Outline = utils.draw('Square',{
+        drawn_objects.Box_Outline = esp.draw('Square',{
             Visible = false, 
             Color = rgb(0,0,0),
             Filled = false,
-            Thickness = 3,
+            Thickness = 3.5,
         });
-        drawn_objects.Box = utils.draw('Square',{
+        drawn_objects.Box = esp.draw('Square',{
             Size = Vec2(0,0),
             Color = esp.box.color,
             Filled = false,
             Thickness = 1,
             Visible = false, 
         });
-        drawn_objects.inner_box = utils.draw('Square',{
+        
+        drawn_objects.health_text = esp.draw('Text',{
             Visible = false, 
-            Size = Vec2(0,0),
-            Filled = true,
-            Thickness = 0,
-            Color = esp.inner_box.color
-        });
-        drawn_objects.Healthbar_Outline = utils.draw('Line',{
+            Size = esp.fontsize,
+            Font = esp.font,
+            Outline = true,
+            Center = false, 
+            Color = esp.health.color,
+        })
+        drawn_objects.Healthbar_Outline = esp.draw('Line',{
             Visible = false,
             Thickness = 3.5,
         });
-        drawn_objects.Healthbar = utils.draw('Line',{
+        drawn_objects.Healthbar = esp.draw('Line',{
             Visible = false,
-            Thickness = 1.5,
+            Thickness = 1,
         });
         esp.objects[obj] = drawn_objects;
     end
     
-    function utils.update_player_esp(obj,array)
+    function esp.update_player_esp(obj,array)
         local character = obj.Character
         if esp.enabled and esp.players and character ~= nil and find_first_child(character,'HumanoidRootPart') ~= nil then
             local hrp = find_first_child(character,'HumanoidRootPart')
 
-            local pos, is_on_screen = wtvp(Camera, hrp.Position)
-            local fov_size = (wtvp(Camera,hrp.Position - Vec3(0, 3, 0)).Y - wtvp(Camera,hrp.Position + Vec3(0, 2.6, 0)).Y) / 2
-            local box_size = Vec2(m_floor(fov_size * 1.8), m_floor(fov_size * 2.3))
-            local box_pos = Vec2(m_floor(pos.X - fov_size * 1.8 / 2), m_floor(pos.Y - fov_size * 1.9/ 2))
+            local Pos, Size = GBB(character)
+			local Height = (Camera.CFrame - Camera.CFrame.p) * Vec3(0, (math.clamp(Size.Y, 1, 10) + 0.5) / 2, 0)
+			Height = math.abs(WTSP(Camera, Pos.Position + Height).Y - WTSP(Camera, Pos.Position - Height).Y)
+			Size = (Vec2(Height / 1.5, Height))
+			local ScreenPosition, OnScreen = wtvp(Camera, Pos.Position)
+			local Position = (Vector2.new(ScreenPosition.X, ScreenPosition.Y) - (Size / 2))
+		
             local bottom_bounds = 0
             local top_bounds = 0 
-            local bottom_offset = Vector2.new(box_size.X / 2 + box_pos.X, box_size.Y + box_pos.Y + 1)
-            local top_offset = Vector2.new(box_size.X / 2 + box_pos.X, box_pos.Y - 16)
-            local distance = math.floor(utils.get_magnitude(hrp.Position,find_first_child(LP.Character,'HumanoidRootPart').Position))
+            local bottom_offset = Vector2.new(Size.X / 2 + Position.X, Size.Y + Position.Y + 1)
+            local top_offset = Vector2.new(Size.X / 2 + Position.X, Position.Y - 16)
+            local distance = math.floor(esp.get_magnitude(hrp.Position,find_first_child(LP.Character,'HumanoidRootPart').Position) / 3.57142857143)
                 
-            if is_on_screen and distance < esp.max_distance_players then
+            if OnScreen and distance < esp.max_distance_players then
                 if esp.highlights.enabled then
                     array.Highlight.Parent = game.GetService(game,'CoreGui') 
                     array.Highlight.Adornee = character
@@ -198,22 +193,7 @@ local utils = {}; do
                     array.Highlight.OutlineTransparency = esp.highlights.outline_transparency
                     array.Highlight.FillTransparency = esp.highlights.fill_transparency
                     array.Highlight.OutlineColor = esp.highlights.outline_color
-                    
-                    --if esp.highlights.chams then 
-                        local parts_array = Camera:GetPartsObscuringTarget({ -- visible check 
-                            character.HumanoidRootPart.Position,
-                            
-                        },{LP.Character,character,workspace.CurrentCamera:FindFirstChild('ViewModel')})
-                        if #parts_array > 1 then
-                            array.Highlight.FillColor = esp.highlights.non_visible
-                        elseif #parts_array < 1 then 
-                                array.Highlight.FillColor = esp.highlights.visible 
-                        end
-                        --else 
-                         --   array.Highlight.FillColor = esp.highlights.fillcolor
-                    --end
-                    else 
-                        array.Highlight.Enabled = false
+                    array.Highlight.FillColor = esp.highlights.fillcolor
                 end
                 if esp.name.enabled then 
                     array.Name.Visible = true 
@@ -223,16 +203,29 @@ local utils = {}; do
                     array.Name.Size = esp.fontsize
                     array.Name.Font = esp.font
                     array.Name.Outline = esp.name.outline
+                    
+                    
                     top_bounds = top_bounds + 14 
                     else 
                         array.Name.Visible = false
                 end
-                    
+                
+                if esp.health.enabled then 
+                    array.health_text.Visible = true 
+                    array.health_text.Position = Vec2((Position.X-30), array.Healthbar.To.Y)
+                    array.health_text.Font = esp.font 
+                    array.health_text.Text = tostring(m_floor(character.Humanoid.Health))
+                    array.health_text.Color = esp.healthbar.lower:lerp(esp.healthbar.higher,character.Humanoid.Health / character.Humanoid.MaxHealth);
+                    array.health_text.Outline = esp.health.outline
+                    else 
+                        array.health_text.Visible = false
+                end
+                
                 if esp.distance.enabled then 
                     array.Distance.Visible = true 
                     array.Distance.Position = Vec2(bottom_offset.X, bottom_offset.Y + bottom_bounds)
                     array.Distance.Color = esp.distance.color 
-                    array.Distance.Text = tostring(distance)..' '..esp.measurement
+                    array.Distance.Text = tostring(m_floor(distance))..' '..esp.measurement
                     array.Distance.Size = esp.fontsize
                     array.Distance.Font = esp.font
                     array.Distance.Outline = esp.distance.outline 
@@ -242,8 +235,8 @@ local utils = {}; do
                 end
                 
                 if esp.inner_box.enabled then 
-                    array.inner_box.Size = Vec2(box_size.X-3,box_size.Y-3)
-                    array.inner_box.Position = Vec2(box_pos.X+1.5,box_pos.Y+1.5)
+                    array.inner_box.Size = Vec2(Size.X-3,Size.Y-3)
+                    array.inner_box.Position = Vec2(Position.X+1.5,Position.Y+1.5)
                     array.inner_box.Visible = true 
                     array.inner_box.Color = esp.inner_box.color 
                     array.inner_box.Transparency = esp.inner_box.transparency
@@ -252,13 +245,13 @@ local utils = {}; do
                 end
                 
                 if esp.box.enabled then 
-                    array.Box.Size = box_size 
-                    array.Box.Position = box_pos
+                    array.Box.Size = Size 
+                    array.Box.Position = Position
                     array.Box.Color = esp.box.color 
                     array.Box.Visible = true 
                     array.Box_Outline.Visible = esp.box.outline 
-                    array.Box_Outline.Size = box_size 
-                    array.Box_Outline.Position = box_pos
+                    array.Box_Outline.Size = Size 
+                    array.Box_Outline.Position = Position
                     else 
                         array.Box.Visible = false 
                         array.Box_Outline.Visible = false
@@ -267,12 +260,12 @@ local utils = {}; do
                 if esp.healthbar.enabled then 
                     array.Healthbar.Visible = true 
                     array.Healthbar.Color = esp.healthbar.lower:lerp(esp.healthbar.higher,character.Humanoid.Health / character.Humanoid.MaxHealth);
-                    array.Healthbar.From = Vec2((box_pos.X - 5), box_pos.Y + box_size.Y)
-                    array.Healthbar.To = Vec2(array.Healthbar.From.X, array.Healthbar.From.Y - (character.Humanoid.Health / character.Humanoid.MaxHealth) * box_size.Y)
+                    array.Healthbar.From = Vec2((Position.X - 5), Position.Y + Size.Y)
+                    array.Healthbar.To = Vec2(array.Healthbar.From.X, array.Healthbar.From.Y - (character.Humanoid.Health / character.Humanoid.MaxHealth) * Size.Y)
                     array.Healthbar_Outline.Visible = esp.healthbar.outline 
                     array.Healthbar_Outline.Color = rgb(0,0,0)
-                    array.Healthbar_Outline.From = Vec2(array.Healthbar.From.X, box_pos.Y + box_size.Y + 1 )
-                    array.Healthbar_Outline.To = Vec2(array.Healthbar.From.X, (array.Healthbar.From.Y -1 * box_size.Y) - 1)
+                    array.Healthbar_Outline.From = Vec2(array.Healthbar.From.X, Position.Y + Size.Y + 1 )
+                    array.Healthbar_Outline.To = Vec2(array.Healthbar.From.X, (array.Healthbar.From.Y -1 * Size.Y) - 1)
                     else 
                         array.Healthbar.Visible = false 
                         array.Healthbar_Outline.Visible = false
@@ -282,7 +275,6 @@ local utils = {}; do
                     array.Tool.Visible = true 
                     array.Tool.Position = Vec2(bottom_offset.X,bottom_offset.Y + bottom_bounds)
                     array.Tool.Size = esp.fontsize 
-                    array.Tool.Color = esp.tool.color
                     array.Tool.Text = tostring(game.GetService(game,'ReplicatedStorage').Players[tostring(obj)].Status.GameplayVariables.EquippedTool.Value)
                     array.Tool.Font = esp.font
                     array.Tool.Outline = esp.tool.outline
@@ -298,12 +290,11 @@ local utils = {}; do
                     array.Distance.Visible = false 
                     array.Box.Visible = false 
                     array.inner_box.Visible = false 
-                    array.Box_Outline.Visible = false
-                    array.Highlight.Enabled = false
+                    array.Box_Outline.Visible = false 
+                    array.health_text.Visible = false
             end
             else 
                 array.inner_box.Visible = false
-                array.Highlight.Enabled = false 
                 array.Tool.Visible = false 
                 array.Healthbar.Visible = false 
                 array.Healthbar_Outline.Visible = false 
@@ -311,29 +302,31 @@ local utils = {}; do
                 array.Distance.Visible = false 
                 array.Box.Visible = false 
                 array.Box_Outline.Visible = false
+                array.health_text.Visible = false
         end
     end
 end
 
 for i,v in next, Players:GetChildren() do
-    if v ~= LP then 
-        utils.add_esp_player(v)
+    if v~=LP then 
+        esp.add_esp_player(v)
     end
 end
 
 Players.PlayerAdded:Connect(function(v)
-    utils.add_esp_player(v)
+    esp.add_esp_player(v)
 end)
 
 Players.PlayerRemoving:Connect(function(v)
-    utils.remove_esp(v)
+    esp.remove_esp(v)
 end)
 RunService.RenderStepped:Connect(function()
     for player, drawings in next, esp.objects do
         if drawings and player then
-            utils.update_player_esp(player, drawings);
+            esp.update_player_esp(player, drawings);
         end
     end
 end)
 
+getgenv().esp = esp 
 return esp
